4067414583480241188ef28a53c4df87d2201ad4
Jason Britto <35361130+john9088@users.noreply.github.com>
2020-07-31 23:23:17 +0530

    Updates

---
+++ precache-manifest.a72c02e4d68018bd5449dde64e693263.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d1a22c2847d0af5cc6cc8bf8f95568",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "ac76bc36a61f1fc776cb",
+    "url": "/react-weather-app/static/css/main.2443ca9b.chunk.css"
+  },
+  {
+    "revision": "a3df884113d712ef3df0",
+    "url": "/react-weather-app/static/js/2.35c49a29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.35c49a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac76bc36a61f1fc776cb",
+    "url": "/react-weather-app/static/js/main.e30e3ec9.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "e702bf5910629e35e76bdff897efe80f",
+    "url": "/react-weather-app/static/media/clear.e702bf59.jpg"
+  },
+  {
+    "revision": "98428059de92420c3f285b986e394a7c",
+    "url": "/react-weather-app/static/media/cloudy.98428059.jpg"
+  },
+  {
+    "revision": "db4b532d5c41990a329ffe82174c4f19",
+    "url": "/react-weather-app/static/media/haze.db4b532d.jpg"
+  },
+  {
+    "revision": "3d149dd78196f9bbdf982cdaaab9588c",
+    "url": "/react-weather-app/static/media/rain.3d149dd7.jpg"
+  },
+  {
+    "revision": "87b3e89542bc8f9b0bb5d5b2b14403b6",
+    "url": "/react-weather-app/static/media/sunny.87b3e895.jpg"
+  }
+]);
